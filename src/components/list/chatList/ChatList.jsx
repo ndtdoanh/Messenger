a2f2216226734a898ab5321c856142e3493652f8
doanh <nguyentrongdoanh202@gmail.com>
2024-11-20 02:14:41 +0700

    first commit

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,6 @@
+import './chatList.css';
+
+const ChatList = () => {
+  return <div className="chatList">ChatList</div>;
+};
+export default ChatList;
---
+++ src/components/list/userInfo/UserInfo.jsx
@@ -0,0 +1,6 @@
+import './userInfo.css';
+
+const UserInfo = () => {
+  return <div className="userInfo">UserInfo</div>;
+};
+export default UserInfo;
